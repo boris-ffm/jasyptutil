2dc3bb213cdc66a74a6f1d257c45b12a5480139a
Boris Donde <boris@dondeweb.de>
2022-06-22 21:21:22 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Easy Tool for Encrypt/Decrypt Strings with jasypt. ##
+\
+**Algoritm:** AES256\
+**Build:** *mvn clean compile assembly:single* \
+**Srart for encrypt:** *java -jar target/jasyptutil-1.0-SNAPSHOT-jar-with-dependencies.jar enc PwdToEncrypt secret \
+**Srart for decrypt:** *java -jar target/jasyptutil-1.0-SNAPSHOT-jar-with-dependencies.jar dec X334XeSe3223e secret \
+
+
+
---
+++ src/main/java/de/donde/jasyptutil/Cryptor.java
@@ -0,0 +1,42 @@
+package de.donde.jasyptutil;
+
+import org.jasypt.util.text.AES256TextEncryptor;
+
+public class Cryptor {
+
+    /**
+     * @param args  1 - enc/dec,  2 - value to encrypt/decrypt,  3 - key
+     */
+    public static void main(String[] args) {
+        if (args.length != 3 && !args[0].equals("enc") && !args[0].equals("dec")) {
+            help();
+            System.exit(-1);
+        }
+        if (args[0].equals("enc")) {
+            System.out.println(encrypt(args[1], args[2]));
+        } else {
+            System.out.println(decrypt(args[1], args[2]));
+        }
+    }
+
+    private static void help() {
+        System.out.println("1 - enc/dec,  2 - value to encrypt,  3 - key");
+    }
+
+    private static AES256TextEncryptor getAes256TextEncryptor(String key) {
+        AES256TextEncryptor textEncryptor = new AES256TextEncryptor();
+        textEncryptor.setPassword(key);
+        return textEncryptor;
+    }
+
+    private static String encrypt(final String value, final String key) {
+        AES256TextEncryptor textEncryptor = getAes256TextEncryptor(key);
+        return textEncryptor.encrypt(value);
+    }
+
+
+    private static String decrypt(final String value, final String key) {
+        AES256TextEncryptor textEncryptor = getAes256TextEncryptor(key);
+        return textEncryptor.decrypt(value);
+    }
+}
