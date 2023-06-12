303bd8e7bf137902099c09eaf01e440bdc618978
Boris Donde <boris.donde.extern@donde.de>
2023-06-12 18:57:44 +0200

    Parameters order changed

--- src/main/java/de/donde/jasyptutil/Cryptor.java
+++ src/main/java/de/donde/jasyptutil/Cryptor.java
@@ -13,18 +13,18 @@ public class Cryptor {
             System.exit(-1);
         }
         System.out.println("Modus:\t" + args[0]);
-        System.out.println("Wert:\t" + args[1]);
-        System.out.println("Key:\t" + args[2]);
+        System.out.println("Wert:\t" + args[2]);
+        System.out.println("Key:\t" + args[1]);
 
         if (args[0].equals("enc")) {
-            System.out.println(encrypt(args[1], args[2]));
+            System.out.println(encrypt(args[2], args[1]));
         } else {
-            System.out.println(decrypt(args[1], args[2]));
+            System.out.println(decrypt(args[2], args[1]));
         }
     }
 
     private static void help() {
-        System.out.println("1 - enc/dec,  2 - value to encrypt,  3 - key");
+        System.out.println("1 - enc/dec, 2 - key, 3 - value to encrypt");
     }
 
     private static AES256TextEncryptor getAes256TextEncryptor(String key) {
